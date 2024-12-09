403481d22c6f3e674a76d43fa1a8d7052518fcd2
KitsuneAlex <alexh@karmakrafts.dev>
2024-12-09 04:32:18 +0100

    Implement bindings

---
+++ multiplatform-bonxai/build.gradle.kts
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2024 Karma Krafts & associates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import de.undercouch.gradle.tasks.download.Download
+import org.gradle.internal.extensions.stdlib.capitalized
+import org.jetbrains.kotlin.gradle.plugin.mpp.DefaultCInteropSettings
+import java.nio.file.Path
+import kotlin.io.path.div
+import kotlin.io.path.exists
+import kotlin.io.path.notExists
+import kotlin.io.path.createDirectories
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.downloadTask)
+    `maven-publish`
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+operator fun DirectoryProperty.div(name: String): Path = get().asFile.toPath() / name
+
+val ensureBuildDirectory: Task = tasks.create("ensureBuildDirectory") {
+    val path = layout.buildDirectory.get().asFile.toPath()
+    doLast { path.createDirectories() }
+    onlyIf { path.notExists() }
+}
+
+fun downloadBonxaiBinariesTask(platform: String, arch: String): Download =
+    tasks.create<Download>("downloadBonxaiBinaries${platform.capitalized()}${arch.capitalized()}") {
+        group = "bonxaiBinaries"
+        dependsOn(ensureBuildDirectory)
+        val fileName = "build-$platform-$arch-debug.zip"
+        src("https://git.karmakrafts.dev/api/v4/projects/349/packages/generic/build/${libs.versions.bonxai.get()}/$fileName")
+        val destPath = layout.buildDirectory / "bonxai" / fileName
+        dest(destPath.toFile())
+        overwrite(true) // Always overwrite when downloading binaries
+        onlyIf { destPath.notExists() }
+    }
+
+val downloadBonxaiBinariesWindowsX64: Download = downloadBonxaiBinariesTask("windows", "x64")
+val downloadBonxaiBinariesLinuxX64: Download = downloadBonxaiBinariesTask("linux", "x64")
+val downloadBonxaiBinariesLinuxArm64: Download = downloadBonxaiBinariesTask("linux", "arm64")
+val downloadBonxaiBinariesMacosX64: Download = downloadBonxaiBinariesTask("macos", "x64")
+val downloadBonxaiBinariesMacosArm64: Download = downloadBonxaiBinariesTask("macos", "arm64")
+
+fun extractBonxaiBinariesTask(platform: String, arch: String): Copy =
+    tasks.create<Copy>("extractBonxaiBinaries${platform.capitalized()}${arch.capitalized()}") {
+        group = "bonxaiBinaries"
+        val downloadTaskName = "downloadBonxaiBinaries${platform.capitalized()}${arch.capitalized()}"
+        dependsOn(downloadTaskName)
+        val platformPair = "$platform-$arch"
+        from(zipTree((layout.buildDirectory / "bonxai" / "build-$platformPair-debug.zip").toFile()))
+        val destPath = layout.buildDirectory / "bonxai" / platformPair
+        into(destPath.toFile())
+        onlyIf { destPath.notExists() }
+    }
+
+val extractBonxaiBinariesWindowsX64: Copy = extractBonxaiBinariesTask("windows", "x64")
+val extractBonxaiBinariesLinuxX64: Copy = extractBonxaiBinariesTask("linux", "x64")
+val extractBonxaiBinariesLinuxArm64: Copy = extractBonxaiBinariesTask("linux", "arm64")
+val extractBonxaiBinariesMacosX64: Copy = extractBonxaiBinariesTask("macos", "x64")
+val extractBonxaiBinariesMacosArm64: Copy = extractBonxaiBinariesTask("macos", "arm64")
+
+val extractBonxaiBinaries: Task = tasks.create("extractBonxaiBinaries") {
+    group = "bonxaiBinaries"
+    dependsOn(extractBonxaiBinariesWindowsX64)
+    dependsOn(extractBonxaiBinariesLinuxX64)
+    dependsOn(extractBonxaiBinariesLinuxArm64)
+    dependsOn(extractBonxaiBinariesMacosX64)
+    dependsOn(extractBonxaiBinariesMacosArm64)
+}
+
+val downloadBonxaiHeaders: Exec = tasks.create<Exec>("downloadBonxaiHeaders") {
+    group = "bonxaiHeaders"
+    dependsOn(ensureBuildDirectory)
+    workingDir = layout.buildDirectory.get().asFile
+    commandLine("git", "clone", "--branch", libs.versions.bonxai.get(), "--single-branch", "https://github.com/karmakrafts/Bonxai", "bonxai/headers")
+    onlyIf { (layout.buildDirectory / "bonxai" / "headers").notExists() }
+}
+
+val updateBonxaiHeaders: Exec = tasks.create<Exec>("updateBonxaiHeaders") {
+    group = "bonxaiHeaders"
+    dependsOn(downloadBonxaiHeaders)
+    workingDir = (layout.buildDirectory / "bonxai" / "headers").toFile()
+    commandLine("git", "pull", "--force")
+    onlyIf { (layout.buildDirectory / "bonxai" / "headers").exists() }
+}
+
+kotlin {
+    listOf(
+        mingwX64(), linuxX64(), linuxArm64(), macosX64(), macosArm64()
+    ).forEach {
+        it.compilations.getByName("main") {
+            cinterops {
+                val boncai by creating {
+                    tasks.getByName(interopProcessingTaskName) {
+                        dependsOn(updateBonxaiHeaders)
+                        dependsOn(extractBonxaiBinaries)
+                    }
+                }
+            }
+        }
+    }
+    applyDefaultHierarchyTemplate()
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.kotlinx.io.core)
+                implementation(libs.kotlinx.io.bytestring)
+            }
+        }
+    }
+}
+
+val dokkaJar by tasks.registering(Jar::class) {
+    dependsOn(tasks.dokkaHtml)
+    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+}
+
+tasks {
+    dokkaHtml {
+        dokkaSourceSets.create("main") {
+            reportUndocumented = false
+            jdkVersion = java.toolchain.languageVersion.get().asInt()
+            noAndroidSdkLink = true
+            externalDocumentationLink("https://docs.karmakrafts.dev${rootProject.name}")
+        }
+    }
+    System.getProperty("publishDocs.root")?.let { docsDir ->
+        create<Copy>("publishDocs") {
+            mustRunAfter(dokkaJar)
+            from(zipTree(dokkaJar.get().outputs.files.first()))
+            into(docsDir)
+        }
+    }
+}
+
+publishing {
+    System.getenv("CI_API_V4_URL")?.let { apiUrl ->
+        repositories {
+            maven {
+                url = uri("$apiUrl/projects/${System.getenv("CI_PROJECT_ID")}/packages/maven")
+                name = "GitLab"
+                credentials(HttpHeaderCredentials::class) {
+                    name = "Job-Token"
+                    value = System.getenv("CI_JOB_TOKEN")
+                }
+                authentication {
+                    create("header", HttpHeaderAuthentication::class)
+                }
+            }
+        }
+    }
+    publications.configureEach {
+        if (this is MavenPublication) {
+            artifact(dokkaJar)
+            pom {
+                name = project.name
+                description = "Multiplatform bindings for the Bonxai sparse voxel grid library on Linux, Windows and macOS."
+                url = System.getenv("CI_PROJECT_URL")
+                licenses {
+                    license {
+                        name = "Apache License 2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "kitsunealex"
+                        name = "KitsuneAlex"
+                        url = "https://git.karmakrafts.dev/KitsuneAlex"
+                    }
+                }
+                scm {
+                    url = this@pom.url
+                }
+            }
+        }
+    }
+}
---
+++ multiplatform-bonxai/src/nativeMain/kotlin/io/karma/bonxai/Bonxai.kt
@@ -0,0 +1,6 @@
+package io.karma.bonxai
+
+/**
+ * @author Alexander Hinze
+ * @since 09/12/2024
+ */
